8818ed9c974098ccabae34e3264690a5278e4133
sweetrocker22 <cdaily87@gmail.com>
2014-07-14 15:59:19 -0500

    Created views and description for each view

---
+++ app/views/homepage.blade.php
@@ -0,0 +1,5 @@
+Homepage
+
+Describes what the website is about
+
+Includes a 'button' that says something like "Start Looking" and goes to index page 
---
+++ app/views/show.blade.php
@@ -0,0 +1,3 @@
+Show Page
+
+This page will show a specific company and their reviews.
