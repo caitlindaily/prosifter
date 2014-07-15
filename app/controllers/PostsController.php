24f6fd72ce3852ba184a1183bc550ad7e8b99cbf
sweetrocker22 <cdaily87@gmail.com>
2014-07-15 16:06:25 -0500

    copied postscontroller and Post model to start

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -2,84 +2,113 @@
 
 class PostsController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
+
+	// public function __construct()
+	// {
+ //    // call base controller constructor
+ //    	parent::__construct();
+
+ //    	$this->beforeFilter('auth', array('except' => array('index', 'show')));
+	// }
+
 	public function index()
-	{
-		//
+	{		
+		$query = Post::with('user');
+
+		if (Input::has('search')) {
+
+			$search = Input::get('search');
+			$query->where('title', 'LIKE', "%$search%");
+		} 
+		$posts = $query->orderBy('created_at', 'desc')->paginate(5);
+		return View::make('posts.index')->with('posts', $posts);
 	}
+        	
 
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
 	public function create()
 	{
-		//
+		return View::make('create-edit');
 	}
 
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
 	public function store()
 	{
-		//
+		//Create instance of validator and use make. Want to validate all input with the rules from Post.
+		$validator = Validator::make(Input::all(), Post::$rules);
+
+		if ($validator->fails()) {
+
+			Session::flash('errorMessage', 'There were errors with your submission.');
+			return Redirect::back()->withInput()->withErrors($validator);
+
+		} else {
+
+			$post = new Post();
+			$post->user_id = Auth::user()->id;
+			$post->title = Input::get('title');
+			$post->body = Input::get('body');
+			$post->slug = Input::get('title');
+			$post->save();
+			if (Input::hasFile('image') && Input::file('image')->isValid()) 
+			{
+				$post->addUploadedImage(Input::file('image'));
+				$post->save();
+			}
+			Session::flash('successMessage', 'Post was created successfully!!!');
+			return Redirect::action('PostsController@index');
+		}
+
 	}
 
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
+	public function show($slug)
+	{	
+		$post = Post::findBySlug($slug);
+		return View::make('posts.show')->with('post', $post);
 	}
 
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
 	{
-		//
+		$post = Post::find($id);
+		return View::make('posts.create-edit')->with('post', $post);
+		
 	}
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
 	{
-		//
+		$post = Post::findOrFail($id);
+		$post->user_id = Auth::user()->id;
+		$validator = Validator::make(Input::all(), Post::$rules);
+
+		if ($validator->fails()) {
+
+			Session::flash('errorMessage', 'There were errors with your update.');
+			return Redirect::back()->withInput()->withErrors($validator);
+
+		} else {
+
+			$post->title = Input::get('title');
+			$post->body = Input::get('body');
+			$post->slug = Input::get('title');
+			$post->save();
+			if (Input::hasFile('image') && Input::file('image')->isValid()) 
+			{
+				$post->addUploadedImage(Input::file('image'));
+				$post->save();
+			}
+			Session::flash('successMessage', 'Your post was updated!!!');
+			return Redirect::action('PostsController@index');
+		}
 	}
 
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
-		//
+		$post = Post::findOrFail($id);
+		$post->delete();
+		Session::flash('successMessage', 'Post was successfully deleted.');
+		return Redirect::action('PostsController@index');
 	}
 
 
