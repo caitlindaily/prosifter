6d999f808654be27ce130db627fb0c2eaee3d47c
sweetrocker22 <cdaily87@gmail.com>
2014-07-21 13:24:41 -0500

    Created function to avg. ratings and then display to provider name

--- app/models/Rating.php
+++ app/models/Rating.php
@@ -4,13 +4,12 @@ class Rating extends BaseModel {
 
 	protected $table = 'ratings';
 
-
 	public function user()
 	{
 		return $this->belongsTo('User');
 	}
 
-	public function providers()
+	public function provider()
 	{
 		return $this->belongsTo('Provider');
 	}
