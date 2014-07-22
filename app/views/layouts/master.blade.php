18ffb3906fe22eb3742c2a89340b133c14180c82
Greg McCabe <gsmccabe1@gmail.com>
2014-07-22 11:19:31 -0500

    added meet the team page

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -138,10 +138,11 @@
 									</a>
 								</li>
 								<li>
-									<a class="dropdown-toggle" href="#">
+									<a class="dropdown-toggle" href="{{ action('HomeController@meetTheTeam') }}">
 										Meet The Team
 									</a>
 								</li>
+
 								@if (Auth::check())
 									@if (Auth::user()->role == 'admin')
 								<li>
